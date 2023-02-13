e0ec978f205df2d78906c609385324df5037f5fa
Fran López <fran2002mc@gmail.com>
2023-02-14 00:58:20 +0100

    Creacion de conexión basica a base de datos local para el inicio de sesion

--- src/Model/Employees.java
+++ src/Model/Employees.java
@@ -1,15 +1,49 @@
 package Model;
 
 import java.util.ArrayList;
+import java.sql.*;
 
 public class Employees {
+
 
     private ArrayList<Employee> employees = new ArrayList<>();
 
     public Employees(){
-        int id = 1;
+        /*int id = 1;
         employees.add(new Employee(id++,"flopez","Contrasena","Fran","López Fenández",50F));
         employees.add(new Employee(id++,"janiebla","Password","José Alberto", "Niebla Fernández", 33f));
+        */
+
+        String url = "jdbc:mysql://localhost:3306/dixho_market";
+        String username = "root";
+        String password = "";
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+
+            Connection connection = DriverManager.getConnection(url, username, password);
+
+            Statement statement = connection.createStatement();
+
+            String query = "SELECT * FROM employees";
+            ResultSet resultSet = statement.executeQuery(query);
+
+            while (resultSet.next()) {
+                employees.add(new Employee(
+                        resultSet.getInt("ID"),
+                        resultSet.getString("user"),
+                        resultSet.getString("password"),
+                        resultSet.getString("name"),
+                        resultSet.getString("surname"),
+                        resultSet.getFloat("discount")
+                ));
+            }
+
+            connection.close();
+        } catch (SQLException | ClassNotFoundException e) {
+            System.out.println("Error al realizar la consulta: " + e.getMessage());
+        }
+
 
     }
 
